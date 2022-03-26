cea0e17043ada905129a1e5c51dd7c4033ea1dab
deepika-mahendra <deepikamahendra805@gmail.com>
2022-03-26 11:14:58 +0530

    dashboard ui

--- frontend/src/dashboard/dashboard.js
+++ frontend/src/dashboard/dashboard.js
@@ -11,6 +11,7 @@ import {
 import DownloadForOfflineIcon from "@mui/icons-material/DownloadForOffline";
 import DeleteIcon from "@mui/icons-material/Delete";
 import Sidenavbar from "../sidenavbar";
+import "./dashboard.css"
 
 function Dashboard(props) {
   return (
@@ -21,32 +22,95 @@ function Dashboard(props) {
         </Col>
         <Col>
           <Container style={{ padding: "15px" }}>
-            <Row className="g-2" style={{ marginBottom: "100px" }}>
-              <Col md>
-                <FloatingLabel
-                  controlId="floatingInputGrid"
-                  label="Email address"
-                >
-                  <Form.Control type="email" placeholder="name@example.com" />
-                </FloatingLabel>
-              </Col>
-              <Col md>
-                <FloatingLabel
-                  controlId="floatingInputGrid"
-                  label="Email address"
-                >
-                  <Form.Control type="email" placeholder="name@example.com" />
-                </FloatingLabel>
-              </Col>
-              <Col md>
-                <FloatingLabel
-                  controlId="floatingInputGrid"
-                  label="Email address"
-                >
-                  <Form.Control type="email" placeholder="name@example.com" />
-                </FloatingLabel>
-              </Col>
+              
+
+            
+            <Row className="g-2" style={{ marginBottom: "50px" }}>
+
+          
+
+            <div class="col-xl-3 col-sm-6 col-12">
+                        <div class="card card-boarder shadow border-0">
+                            <div class="card-body-dash">
+                                <div class="row">
+                                    <div class="col">
+                                        <span class="h6 font-semibold text-muted text-sm d-block mb-2">Document Title</span>
+                                        <span class="h3 font-bold mb-0">$750.90</span>
+                                    </div>
+                                    <div class="col-auto">
+                                        <div class="icon icon-shape bg-tertiary text-white text-lg rounded-circle">
+                                            <i class="bi bi-credit-card"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                               
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-xl-3 col-sm-6 col-12">
+                        <div class="card card-boarder shadow border-0">
+                            <div class="card-body-dash">
+                                <div class="row">
+                                    <div class="col">
+                                        <span class="h6 font-semibold text-muted text-sm d-block mb-2">Document Title</span>
+                                        <span class="h3 font-bold mb-0">$750.90</span>
+                                    </div>
+                                    <div class="col-auto">
+                                        <div class="icon icon-shape bg-tertiary text-white text-lg rounded-circle">
+                                            <i class="bi bi-credit-card"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                               
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-xl-3 col-sm-6 col-12">
+                        <div class="card card-boarder shadow border-0">
+                            <div class="card-body-dash">
+                                <div class="row">
+                                    <div class="col">
+                                        <span class="h6 font-semibold text-muted text-sm d-block mb-2">Document Title</span>
+                                        <span class="h3 font-bold mb-0">$750.90</span>
+                                    </div>
+                                    <div class="col-auto">
+                                        <div class=" icon-shape bg-tertiary text-white text-lg rounded-circle">
+                                            <i class="bi bi-credit-card"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                               
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-sm-6 col-12">
+                        <div class="card card-boarder shadow border-0">
+                            <div class="card-body-dash">
+                                <div class="row">
+                                    <div class="col">
+                                        <span class="h6 font-semibold text-muted text-sm d-block mb-2">Document Title</span>
+                                        <span class="h3 font-bold mb-0">$750.90</span>
+                                    </div>
+                                    <div class="col-auto">
+                                        <div class="icon icon-shape bg-tertiary text-white text-lg rounded-circle">
+                                            <i class="bi bi-credit-card"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                               
+                            </div>
+                        </div>
+                    </div>
+
             </Row>
+
+
+          
+<Container className="All-document">
+  <h3 className="H-Document">Documents</h3>
+
             <ListGroup variant="flush">
               <ListGroup.Item>
                 <Row style={{ width: "100%" }}>
@@ -57,7 +121,7 @@ function Dashboard(props) {
                     </Card.Subtitle>
                   </Col>
                   <Col md={1} style={{ float: "right" }}>
-                    <DownloadForOfflineIcon />
+                   <DownloadForOfflineIcon />
                     <DeleteIcon />
                   </Col>
                 </Row>
@@ -74,7 +138,10 @@ function Dashboard(props) {
                     <DownloadForOfflineIcon />
                     <DeleteIcon />
                   </Col>
+                  
+                 
                 </Row>
+               
               </ListGroup.Item>
               <ListGroup.Item>
                 <Row style={{ width: "100%" }}>
@@ -105,11 +172,13 @@ function Dashboard(props) {
                 </Row>
               </ListGroup.Item>
             </ListGroup>
+            </Container>
+
           </Container>
         </Col>
       </Row>
     </>
   );
-}
+    }
 
 export default Dashboard;
--- frontend/src/sidenavbar.js
+++ frontend/src/sidenavbar.js
@@ -1,43 +1,33 @@
 import React from "react";
-import { Col, Figure, Row, Button, Stack } from "react-bootstrap";
+import { ButtonGroup, Col, Figure, Row, Button, Stack } from "react-bootstrap";
 import profilePic from "./image/profilePic.jpg";
-import "./App.css";
+import './App.css'
 
 function Sidenavbar(props) {
   return (
     <>
-      <Col className="sidenavbar" style={{ height: "100vh" }}>
+      <Col  className="sidenavbar" style={{ height: "100vh" }}>
         <Row>
-          <Figure style={{ borderRadius: "50%" }}>
+          <Figure>
             <Figure.Image
               width={171}
               height={180}
               alt="171x180"
+              style={{borderRadius:"50%"}}
               src={profilePic}
             />
-            <Figure.Caption>Company Name</Figure.Caption>
+            <Figure.Caption>
+              Company Name
+            </Figure.Caption>
           </Figure>
         </Row>
-        <Row
-          style={{
-            textAlign: "center",
-            alignContent: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Stack gap={4} style={{ marginTop: "100px" }}>
-            <a href="/dashboard" className="tabs">Dashboard</a>
-            <div className="tabs">Important</div>
-            <div className="tabs">Sent</div>
-          </Stack>
-          <Button
-            variant="light"
-            className="btns"
-            style={{ marginTop: "20vh" }}
-            onClick={()=>{window.location.href="/create"}}
-          >
-            Create
-          </Button>
+        <Row style={{textAlign:"center", alignContent:"center", justifyContent:"center"}}>
+        <Stack gap={4} style={{marginTop: "100px"}}>
+                        <div className="tabs" >Dashboard</div>
+                        <div className="tabs" >Important</div>
+                        <div className="tabs" >Sent</div>
+                    </Stack>
+        <Button variant="light" className="btns" style={{marginTop:"20vh"}}>Create</Button>
         </Row>
       </Col>
     </>
